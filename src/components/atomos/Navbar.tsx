a01b4aac227286c10231411b5423d3c9b1266b6b
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-01-25 18:33:00 -0300

    changes

--- src/components/atomos/Navbar.tsx
+++ src/components/atomos/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import LoginRegisterModal from "./Modal";
 import { FaUser, FaChevronDown, FaChevronUp } from "react-icons/fa";
