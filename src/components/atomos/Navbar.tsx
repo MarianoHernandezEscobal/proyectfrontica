760d39fcf3a196a480b5de774f1987e36a8ace2a
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-01-25 18:28:37 -0300

    navbar

--- src/components/atomos/Navbar.tsx
+++ src/components/atomos/Navbar.tsx
@@ -1,35 +1,40 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import LoginRegisterModal from "./Modal";
 import { FaUser, FaChevronDown, FaChevronUp } from "react-icons/fa";
-import logo from "../../assets/imgs/logo.png";
-import { hasCookie } from "../../utils/cookie";
-import { logoutUser } from "../../services/users/userService";
 import { HiMenuAlt3 } from "react-icons/hi";
+
+import logo from "../../assets/imgs/logo.png";
 import { useAlert } from "../../contexts/AlertContext";
-import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext"; // <-- Import from AuthContext
 
 const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  // Local UI states
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [hasScrolled, setHasScrolled] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+
+  // Refs
   const navRef = useRef<HTMLDivElement>(null);
   const userMenuRef = useRef<HTMLDivElement>(null);
-  const { showAlert } = useAlert();
-  const { user } = useAuth();
 
+  // Contexts
+  const { showAlert } = useAlert();
+  const { user, isAuthenticated, logoutUser } = useAuth(); // <-- AuthContext
+
+  // Close the modal
   const handleModalClose = useCallback(() => {
     setIsModalOpen(false);
   }, []);
 
+  // Logout
   const handleLogout = async () => {
     try {
-      await logoutUser();
-      setIsLoggedIn(false);
+      await logoutUser(); // <-- Calls logout from AuthContext
       setIsNavOpen(false);
       setIsUserMenuOpen(false);
       showAlert("info", "Sesión cerrada correctamente");
@@ -40,31 +45,22 @@ const Navbar = () => {
     }
   };
 
+  // Toggle user menu or open login modal
   const toggleUserMenu = useCallback(() => {
-    if (isLoggedIn) 
-      {
-        console.log('isLoggedIn', isLoggedIn);
-        console.log('cookie', hasCookie("sessionIndicator"));
-
+    if (isAuthenticated) {
       setIsUserMenuOpen((prev) => !prev);
     } else {
       setIsModalOpen(true);
     }
-  }, [isLoggedIn]);
+  }, [isAuthenticated]);
 
+  // Close all menus
   const closeMenus = useCallback(() => {
     setIsNavOpen(false);
     setIsUserMenuOpen(false);
   }, []);
 
-  useEffect(() => {
-    setIsLoggedIn(hasCookie("sessionIndicator"));
-  }, []);
-
-  useEffect(() => {
-    setIsLoggedIn(hasCookie("sessionIndicator"));
-  }, [isModalOpen]);
-
+  // Close menus if user clicks outside
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -90,10 +86,12 @@ const Navbar = () => {
     };
   }, [closeMenus]);
 
+  // Close menus on route change
   useEffect(() => {
     closeMenus();
   }, [location, closeMenus]);
 
+  // Utility to determine if a link is active
   const isActive = useCallback(
     (path: string) =>
       location.pathname === path
@@ -102,6 +100,7 @@ const Navbar = () => {
     [location.pathname]
   );
 
+  // Nav links
   const navLinks = [
     { path: "/home", label: "Inicio" },
     { path: "/properties", label: "Propiedades" },
@@ -109,12 +108,14 @@ const Navbar = () => {
     { path: "/properties/create", label: "Publicar Propiedad" },
   ];
 
+  // Items in the user dropdown
   const userMenuItems = [
     { path: "/profile", label: "Perfil" },
     { path: "/properties/created", label: "Mis Propiedades" },
     { path: "/properties/favourites", label: "Mis Favoritas" },
   ];
 
+  // Extra items if user is admin
   const adminMenuItems = [
     ...userMenuItems,
     { path: "/properties/pending-approval", label: "Pendiente de aprobación" },
@@ -125,15 +126,18 @@ const Navbar = () => {
     <nav
       ref={navRef}
       className={`fixed top-0 left-0 right-0 z-40 p-2 pr-3 bg-gradient-to-b from-accent-light to-background-neutral transition-all duration-300 ${
-        (location.pathname === "/home" || location.pathname === '' || location.pathname === '/' )&& !hasScrolled
+        (location.pathname === "/home" || location.pathname === "" || location.pathname === "/") && !hasScrolled
           ? "opacity-0 invisible"
           : "opacity-100 visible"
       }`}
     >
       <div className="flex justify-between items-center">
+        {/* Logo */}
         <Link to="/home">
           <img src={logo} alt="Logo" width="120" height="50" className="mr-4" />
         </Link>
+
+        {/* Desktop Navigation */}
         <div className="hidden md:flex gap-3 items-center">
           {navLinks.map(({ path, label }) => (
             <Link
@@ -144,31 +148,29 @@ const Navbar = () => {
               {label}
             </Link>
           ))}
+
+          {/* User Menu */}
           <div className="relative" ref={userMenuRef}>
             <button
-              className={`nav-button  text-text-light  hover:font-bold transition-text duration-300 flex items-center ${
+              className={`nav-button text-text-light hover:font-bold transition-text duration-300 flex items-center ${
                 isUserMenuOpen ? "text-text-light" : ""
               }`}
               onClick={toggleUserMenu}
               aria-label="Perfil de usuario"
             >
               <FaUser className="mr-2" />
-              {isLoggedIn ? "Mi Cuenta" : "Iniciar Sesión"}
-              {isUserMenuOpen ? (
-                <FaChevronUp className="ml-2" />
-              ) : (
-                <FaChevronDown className="ml-2" />
-              )}
+              {isAuthenticated ? "Mi Cuenta" : "Iniciar Sesión"}
+              {isUserMenuOpen ? <FaChevronUp className="ml-2" /> : <FaChevronDown className="ml-2" />}
             </button>
-            {isUserMenuOpen && isLoggedIn && (
+
+            {/* Dropdown */}
+            {isUserMenuOpen && isAuthenticated && (
               <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 z-50">
                 {user?.admin
                   ? adminMenuItems.map(({ path, label }) => (
                       <Link
                         key={path}
-                        className={`block px-4 py-2 text-sm text-text-primary hover:bg-background-neutral ${isActive(
-                          path
-                        )}`}
+                        className={`block px-4 py-2 text-sm text-text-primary hover:bg-background-neutral ${isActive(path)}`}
                         to={path}
                         onClick={closeMenus}
                       >
@@ -178,9 +180,7 @@ const Navbar = () => {
                   : userMenuItems.map(({ path, label }) => (
                       <Link
                         key={path}
-                        className={`block px-4 py-2 text-sm text-text-primary hover:bg-background-neutral ${isActive(
-                          path
-                        )}`}
+                        className={`block px-4 py-2 text-sm text-text-primary hover:bg-background-neutral ${isActive(path)}`}
                         to={path}
                         onClick={closeMenus}
                       >
@@ -197,6 +197,8 @@ const Navbar = () => {
             )}
           </div>
         </div>
+
+        {/* Mobile Navigation Button */}
         <button
           className="md:hidden nav-button text-2xl text-text-light"
           onClick={() => setIsNavOpen((prev) => !prev)}
@@ -205,8 +207,10 @@ const Navbar = () => {
           <HiMenuAlt3 />
         </button>
       </div>
+
+      {/* Mobile Nav Links */}
       {isNavOpen && (
-        <div className="md:hidden flex flex-col gap-3 mt-3 text-right justify-end items-end  p-4 rounded-md">
+        <div className="md:hidden flex flex-col gap-3 mt-3 text-right justify-end items-end p-4 rounded-md">
           {navLinks.map(({ path, label }) => (
             <Link
               key={path}
@@ -217,6 +221,8 @@ const Navbar = () => {
               {label}
             </Link>
           ))}
+
+          {/* Mobile User Menu Toggle */}
           <button
             className={`nav-button transition-text duration-300 text-right hover:font-bold hover:text-text-light flex items-center ${
               isUserMenuOpen ? "text-text-light" : ""
@@ -225,31 +231,44 @@ const Navbar = () => {
             aria-label="Perfil de usuario"
           >
             <FaUser className="mr-2" />
-            {isLoggedIn ? "Mi Cuenta" : "Iniciar Sesión"}
+            {isAuthenticated ? "Mi Cuenta" : "Iniciar Sesión"}
             <FaChevronDown className="ml-2" />
           </button>
-          {isLoggedIn && isUserMenuOpen && (
+
+          {/* Mobile Dropdown for Authenticated Users */}
+          {isAuthenticated && isUserMenuOpen && (
             <>
-              {userMenuItems.map(({ path, label }) => (
-                <Link
-                  key={path}
-                  className={`nav-button ${isActive(path)} pl-8`}
-                  to={path}
-                  onClick={closeMenus}
-                >
-                  {label}
-                </Link>
-              ))}
-              <button
-                className="nav-button hover:bg-accent-light text-left pl-8"
-                onClick={handleLogout}
-              >
+              {user?.admin
+                ? adminMenuItems.map(({ path, label }) => (
+                    <Link
+                      key={path}
+                      className={`nav-button ${isActive(path)} pl-8`}
+                      to={path}
+                      onClick={closeMenus}
+                    >
+                      {label}
+                    </Link>
+                  ))
+                : userMenuItems.map(({ path, label }) => (
+                    <Link
+                      key={path}
+                      className={`nav-button ${isActive(path)} pl-8`}
+                      to={path}
+                      onClick={closeMenus}
+                    >
+                      {label}
+                    </Link>
+                  ))}
+
+              <button className="nav-button hover:bg-accent-light text-left pl-8" onClick={handleLogout}>
                 Cerrar Sesión
               </button>
             </>
           )}
         </div>
       )}
+
+      {/* Modal for Login/Register */}
       <LoginRegisterModal isOpen={isModalOpen} onClose={handleModalClose} />
     </nav>
   );
