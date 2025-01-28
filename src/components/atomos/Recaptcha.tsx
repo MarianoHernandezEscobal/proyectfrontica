ac25190a1c89c6647b7ecacc8011ec937a1e3e5c
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-01-28 09:11:38 -0300

    change apiKey recaptcha

--- src/components/atomos/Recaptcha.tsx
+++ src/components/atomos/Recaptcha.tsx
@@ -3,7 +3,7 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import { RecaptchaProps } from '../../utils/types';
 
 const Recaptcha: React.FC<RecaptchaProps> = ({ onError, setIsChecked }) => {
-  const siteKey = '6Leqab0qAAAAAPi3LR22HjWVGOwFpZbXFhmu7lO_';
+  const siteKey = '6Leqab0qAAAAAGz3kpY6bzIv_wa2WijnD3OprEIh';
 
   const handleChange = (value: string | null) => {
     if (value) {
