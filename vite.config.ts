c3191739d577d9b1344af60196fbf7df19b57c12
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-01-28 20:52:46 -0300

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  publicDir: 'public', // Specify the public directory
+  root: './', // Ensure the root is correctly set
   build: {
     rollupOptions: {
-      input: "./index.html",
+      input: './public/index.html', // Explicitly specify the entry point
     },
   },
 });
